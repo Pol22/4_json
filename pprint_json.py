9b23114f5d9f0f0f961a26b0f4b2d3eea8119426
Pavel <pashazor22@gmail.com>
2017-02-20 17:48:35 +0300

    simple functions

--- pprint_json.py
+++ pprint_json.py
@@ -10,6 +10,35 @@ def load_json_data(filepath):
         return json.load(file_handler)
 
 
+def pretty_dict(data, number_tabs):
+    print('{')
+    dict_keys = sorted(data.keys())
+    for key in dict_keys:
+        print('\t'*number_tabs, end='')
+        pretty_print(key, number_tabs+1)
+        print(': ', end='')
+        pretty_print(data[key], number_tabs+1)
+        if key == dict_keys[-1]:
+            print('')
+        else:
+            print(',')
+    print('\t'*(number_tabs-1), end='')
+    print('}', end='')
+
+
+def pretty_list(data, number_tabs):
+    print('[')
+    for item in data:
+        print('\t'*number_tabs, end='')
+        pretty_print(item, number_tabs+1)
+        if item == data[-1]:
+            print('')
+        else:
+            print(',')
+    print('\t'*(number_tabs-1), end='')
+    print(']', end='')
+
+
 def pretty_print(data, number_tabs):
     if isinstance(data, int) or isinstance(data, float):
         print(data, end='', sep='')
@@ -18,31 +47,10 @@ def pretty_print(data, number_tabs):
         print("\""+data+"\"", end='', sep='')
 
     if isinstance(data, dict):
-        print('{')
-        dict_keys = sorted(data.keys())
-        for key in dict_keys:
-            print('\t'*number_tabs, end='')
-            pretty_print(key, number_tabs+1)
-            print(': ', end='')
-            pretty_print(data[key], number_tabs+1)
-            if key == dict_keys[-1]:
-                print('')
-            else:
-                print(',')
-        print('\t'*(number_tabs-1), end='')
-        print('}', end='')
+        pretty_dict(data, number_tabs)
 
     if isinstance(data, list):
-        print('[')
-        for item in data:
-            print('\t'*number_tabs, end='')
-            pretty_print(item, number_tabs+1)
-            if item == data[-1]:
-                print('')
-            else:
-                print(',')
-        print('\t'*(number_tabs-1), end='')
-        print(']', end='')
+        pretty_list(data, number_tabs)
 
 
 if __name__ == '__main__':
