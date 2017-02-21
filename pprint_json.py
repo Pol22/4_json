46e3f4fc949fc84a8ff37912d5bc84b61957ea12
Pavel <pashazor22@gmail.com>
2017-02-21 16:48:43 +0300

    New version

--- pprint_json.py
+++ pprint_json.py
@@ -1,68 +1,81 @@
 import json
 import os
-import sys
+import argparse
 
 
 def load_json_data(filepath):
     if not os.path.exists(filepath):
         return None
-    with open(filepath, 'r') as file_handler:
+
+    with open(filepath, 'r', encoding='utf8') as file_handler:
         return json.load(file_handler)
 
 
-def pretty_print_dict(data, number_tabs):
-    print('{')
+def get_pretty_dict(data, number_tabs):
+    return_str = '{\n'
     dict_keys = sorted(data.keys())
     for key in dict_keys:
-        print('\t'*number_tabs, end='')
-        pretty_print_json(key, number_tabs+1)
-        print(': ', end='')
-        pretty_print_json(data[key], number_tabs+1)
+        return_str += '\t' * number_tabs
+        return_str += get_pretty_json(key, number_tabs+1)
+        return_str += ': '
+        return_str += get_pretty_json(data[key], number_tabs+1)
         if key == dict_keys[-1]:
-            print('')
+            return_str += '\n'
         else:
-            print(',')
-    print('\t'*(number_tabs-1), end='')
-    print('}', end='')
+            return_str += ',\n'
+    return_str += '\t' * (number_tabs - 1)
+    return_str += '}'
+    return return_str
 
 
-def pretty_print_list(data, number_tabs):
-    print('[')
+def get_pretty_list(data, number_tabs):
+    return_str = '[\n'
     for item in data:
-        print('\t'*number_tabs, end='')
-        pretty_print_json(item, number_tabs+1)
+        return_str += '\t' * number_tabs
+        return_str += get_pretty_json(item, number_tabs+1)
         if item == data[-1]:
-            print('')
+            return_str += '\n'
         else:
-            print(',')
-    print('\t'*(number_tabs-1), end='')
-    print(']', end='')
+            return_str += ',\n'
+    return_str += '\t' * (number_tabs - 1)
+    return_str += ']'
+    return return_str
 
 
-def pretty_print_json(data, number_tabs):
+def get_pretty_json(data, number_tabs):
+    result_str = ''
+    if data is None:
+        result_str += 'null'
+
     if isinstance(data, int) or isinstance(data, float):
-        print(data, end='', sep='')
+        result_str += str(data)
 
     if isinstance(data, str):
-        print("\""+data+"\"", end='', sep='')
+        result_str += "\"" + data + "\""
 
     if isinstance(data, dict):
-        pretty_print_dict(data, number_tabs)
+        result_str += get_pretty_dict(data, number_tabs)
 
     if isinstance(data, list):
-        pretty_print_list(data, number_tabs)
+        result_str += get_pretty_list(data, number_tabs)
+
+    return result_str
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Input path to the json file second argument")
-        exit()
-    filepath = sys.argv[1]
+    parser = argparse.ArgumentParser(description='Printing pretty some json')
+    parser.add_argument('filepath', metavar='filepath', type=str, nargs=1,
+                        help='path to json file')
+
+    args = parser.parse_args()
+    filepath = args.filepath[0]
 
     if not os.path.exists(filepath):
         print("Incorrect path")
-        exit()
+        exit(-1)
 
     json_content = load_json_data(filepath)
 
-    pretty_print_json(json_content, 1)
+    pretty_json = get_pretty_json(json_content, 1)
+
+    print(pretty_json)
