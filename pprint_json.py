51e180e3d396a53c491706e85bf017751618be38
Pavel <pashazor22@gmail.com>
2017-02-21 14:14:41 +0300

    md

--- pprint_json.py
+++ pprint_json.py
@@ -10,14 +10,14 @@ def load_json_data(filepath):
         return json.load(file_handler)
 
 
-def pretty_dict(data, number_tabs):
+def pretty_print_dict(data, number_tabs):
     print('{')
     dict_keys = sorted(data.keys())
     for key in dict_keys:
         print('\t'*number_tabs, end='')
-        pretty_print(key, number_tabs+1)
+        pretty_print_json(key, number_tabs+1)
         print(': ', end='')
-        pretty_print(data[key], number_tabs+1)
+        pretty_print_json(data[key], number_tabs+1)
         if key == dict_keys[-1]:
             print('')
         else:
@@ -26,11 +26,11 @@ def pretty_dict(data, number_tabs):
     print('}', end='')
 
 
-def pretty_list(data, number_tabs):
+def pretty_print_list(data, number_tabs):
     print('[')
     for item in data:
         print('\t'*number_tabs, end='')
-        pretty_print(item, number_tabs+1)
+        pretty_print_json(item, number_tabs+1)
         if item == data[-1]:
             print('')
         else:
@@ -39,7 +39,7 @@ def pretty_list(data, number_tabs):
     print(']', end='')
 
 
-def pretty_print(data, number_tabs):
+def pretty_print_json(data, number_tabs):
     if isinstance(data, int) or isinstance(data, float):
         print(data, end='', sep='')
 
@@ -47,10 +47,10 @@ def pretty_print(data, number_tabs):
         print("\""+data+"\"", end='', sep='')
 
     if isinstance(data, dict):
-        pretty_dict(data, number_tabs)
+        pretty_print_dict(data, number_tabs)
 
     if isinstance(data, list):
-        pretty_list(data, number_tabs)
+        pretty_print_list(data, number_tabs)
 
 
 if __name__ == '__main__':
@@ -65,4 +65,4 @@ if __name__ == '__main__':
 
     json_content = load_json_data(filepath)
 
-    pretty_print(json_content, 1)
+    pretty_print_json(json_content, 1)
